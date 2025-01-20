c238f34850a83cf07702f41bd21911db81cdbcdf
Ryan <98045919+Woodedcode@users.noreply.github.com>
2025-01-19 22:57:20 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,143 @@
+//page31
+// let nr1 = 2;
+// let nr2 = "2";
+// console.log(nr1 + Number(nr2)) 
+
+//page32
+// let str1 = 'Ryan'
+// console.log(str1, typeof str1)
+// let str2 = "Woods";
+// console.log(str2, typeof str2)
+// let val1 = undefined;
+// console.log(val1, typeof val1)
+// let val2 = null;
+// console.log(val2, typeof val2)
+// let myNum = 1000;
+// console.log(myNum, typeof myNum)
+// let nr1 = 12;
+// let nr2 = 14;
+// console.log(nr1 + nr2)
+
+// let str1 = "Hello ";
+// let str2 = "Addition";
+// console.log(str1 + str2)
+
+//page33
+// let name = "Ryan";
+// let age = "35";
+// let js = true;
+// console.log('Hello, my name is ' + name + ", I am " + age + " years old and I can code JavaScript: " + js)
+
+// let nr1 = 20;
+// let nr2 = 4;
+// let str1 = "Hi";
+// let nr3 = 3;
+// let result1 = nr1 - nr2;
+// let result2 = str1 - nr3
+// console.log(result1, result2)
+
+//page34
+// let nr1 = 15;
+// let nr2 = 10;
+// let str1 = "Hi";
+// let nr3 = 3;
+// let result1 = nr1 * nr2;
+// let result2 = str1 * nr3
+// console.log(result1, result2)
+
+// let nr1 = 30;
+// let nr2 = 5;
+// let result1 = nr1 / nr2;
+// console.log(result1)
+
+// let nr1 = 2;
+// let nr2 = 3
+// let result1 = nr1 ** nr2;
+// console.log(result1)
+
+
+//page35
+// let nr1 = 10;
+// let nr2 = 3;
+// let result1 = nr1 % nr2
+// console.log(`${nr1} % ${nr2} = ${result1}`)
+
+// let nr3 = 8;
+// let nr4 = 2;
+// let result2 = nr3 % nr4
+// console.log(`${nr3} % ${nr4} = ${result2}`)
+
+// let nr5 = 15;
+// let nr6 = 4;
+// let result3 = nr5 % nr6;
+// console.log(`${nr5} % ${nr6} = ${result3}`)
+
+//page36
+// let nr1 = 4;
+// nr1++
+// console.log(nr1)
+
+// let nr2 = 4
+// nr2--
+// console.log(nr2)
+
+//page37
+// let nr = 2;
+// console.log(nr++)
+// console.log(nr)
+// console.log(++nr)
+// let nr1 = 4;
+// let nr2 = 5;
+// let nr3 = 2;
+// console.log(nr1++ + ++nr2 * nr3++)
+
+//page38
+// console.log(Math.hypot(6, 8))
+
+//page39
+// let x = 2;
+// x += 2
+// x -= 2
+// x *= 6
+// x /= 3
+// x ** 2
+// x %= 3
+// console.log(x)
+
+// //page40
+// let a = 3;
+// let b = 4;
+// let c = 5;
+
+// console.log('ADDITION', a + b);
+// console.log('ADDITION', a / c);
+// console.log('MODULUS', c = c % b);
+
+// let x = 5;
+// let y = "5";
+// console.log(x == y)
+// console.log(x === y)
+// console.log(x != y)
+// console.log(x !== y)
+
+//page41
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
